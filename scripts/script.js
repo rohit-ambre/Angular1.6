a5eebabec2019738294073dc9187de730307984f
Rohit Ambre <rohit003.ambre@gmail.com>
2017-11-21 06:04:14 +0000

    used filters and rowlimit

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,20 @@
 var myApp= angular
 				.module("myModule",[])
 				.controller("myController" , function($scope){
-	
-		var technologies =[
-			{name:"C#", likes:0, dislikes:0},
-			{name:"Node", likes:0, dislikes:0},
-			{name:"Perl", likes:0, dislikes:0},	
-			{name:"ASP", likes:0, dislikes:0},
+
+
+		var employees =[
+			{name:"Rohit", dateOfBirth: new Date("November 29, 1994"), gender:"Male", salary:28000},
+			{name:"Shubham", dateOfBirth: new Date("Octobrer 12, 1995"), gender:"Male", salary:32000},
+			{name:"Suresh", dateOfBirth: new Date("August 24, 1996"), gender:"Female", salary:18000},
+			{name:"Sandesh", dateOfBirth: new Date("December 04, 1993"), gender:"Male", salary:38000},
 
 		];
 
-	$scope.technologies=technologies;
-
-	$scope.incrementlikes= function(technology){
-		technology.likes++;
-	}
+	$scope.employees=employees;
+	$scope.rowLimit=3;
 
 
-	$scope.incrementdislikes= function(technology){
-		technology.dislikes++;
-	}
 });
 
 // var myController = function($scope){
