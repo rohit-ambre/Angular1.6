d352d7ee5f1a8009035e9ac6a6d60dbd94567fee
Rohit Ambre <rohit003.ambre@gmail.com>
2017-11-21 06:16:57 +0000

    sort by name

--- scripts/script.js
+++ scripts/script.js
@@ -9,13 +9,14 @@ var myApp= angular
 		var employees =[
 			{name:"Rohit", dateOfBirth: new Date("November 29, 1994"), gender:"Male", salary:28000},
 			{name:"Shubham", dateOfBirth: new Date("Octobrer 12, 1995"), gender:"Male", salary:32000},
-			{name:"Suresh", dateOfBirth: new Date("August 24, 1996"), gender:"Female", salary:18000},
+			{name:"Arjun", dateOfBirth: new Date("August 24, 1996"), gender:"Female", salary:18000},
 			{name:"Sandesh", dateOfBirth: new Date("December 04, 1993"), gender:"Male", salary:38000},
 
 		];
 
 	$scope.employees=employees;
-	$scope.rowLimit=3;
+	$scope.sortColumn="name";
+
 
 
 });
