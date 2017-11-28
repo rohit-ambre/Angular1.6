31fe76aa5d49b52a530bc4a2ae740e963a82f7f3
Rohit Ambre <rohit003.ambre@gmail.com>
2017-11-28 11:18:08 +0000

    header sort for name column

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,22 @@ var myApp= angular
 
 	$scope.employees=employees;
 	$scope.sortColumn="name";
+	$scope.reverseSort=false;
+
+	$scope.sortData= function(column){
+
+		$scope.reverseSort=($scope.sortColumn== column) ? !$scope.reverseSort : false;
+		$scope.sortColumn = column;
+	}
+
+	$scope.getsortClass=function(column){
+		if($scope.sortColumn==column){
+			return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+		}
+
+		return '';
+
+	}
 
 
 
